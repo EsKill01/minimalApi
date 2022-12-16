a05cf51326af9c606403c0a4efa2af473a2ce721
EsKill01 <edbarboza20@gmail.com>
2022-12-15 22:56:45 -0600

    Add last change

--- MinimalAPIDemo/MagicalVilla_CoponAPI/Data/ApplicationDbContext.cs
+++ MinimalAPIDemo/MagicalVilla_CoponAPI/Data/ApplicationDbContext.cs
@@ -1,6 +1,40 @@
-﻿namespace MagicalVilla_CoponAPI.Data
+﻿using MagicalVilla_CoponAPI.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicalVilla_CoponAPI.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Coupon>  Coupons { get; set; }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                Id= 1,
+                Created = DateTime.Now,
+                IsActive= true,
+                Name= "DDDF",
+                Percent= 12
+            },
+            new Coupon()
+            {
+                Id= 2,
+                Created = DateTime.Now,
+                IsActive= true,
+                Name="EEFFF",
+                Percent= 55
+            }
+            )
+                ;
+        }
+
+        
+
     }
 }
